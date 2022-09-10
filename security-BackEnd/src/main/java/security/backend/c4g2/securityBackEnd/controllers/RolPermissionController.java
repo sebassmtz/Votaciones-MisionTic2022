3afd6199a9c3fdcba21510c80ac1c49890b73ee6
sebastian <sebastian.martinez07@uptc.edu.co>
2022-09-09 19:16:37 -0500

    ENDPOINTS BACKEND SEGURITY

--- security-BackEnd/src/main/java/security/backend/c4g2/securityBackEnd/controllers/RolPermissionController.java
+++ security-BackEnd/src/main/java/security/backend/c4g2/securityBackEnd/controllers/RolPermissionController.java
@@ -41,6 +41,30 @@ public class RolPermissionController {
         return  null;
     }
 
+    @GetMapping("{id}")
+    public RolPermission show(@PathVariable String id){
+        return rolPermissionRepository.findById(id).orElse(null);
+    }
 
+    @PutMapping("{id}/rol/{id_rol}/permiso/{id_permission}")
+    public RolPermission update(@PathVariable String id,@PathVariable String id_rol, @PathVariable String id_permission){
+        RolPermission rolPermission = rolPermissionRepository.findById(id).orElse(null);
+        Rol rol = rolRepository.findById(id_rol).orElse(null);
+        Permission permission = permissionRepository.findById(id_permission).orElse(null);
+        if (rolPermission != null && rol != null && permission != null) {
+            rolPermission.setRol(rol);
+            rolPermission.setPermission(permission);
+            return rolPermissionRepository.save(rolPermission);
+        }
+        return null;
+    }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id){
+        RolPermission rolPermission = rolPermissionRepository.findById(id).orElse(null);
+        if (rolPermission != null) {
+            rolPermissionRepository.delete(rolPermission);
+        }
+    }
 }
